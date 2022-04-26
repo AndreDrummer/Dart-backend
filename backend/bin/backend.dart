f4a55c355700f84383db222815fdbae261e2e3bd
Andre Silva <academicworks.adf@gmail.com>
2022-04-25 22:20:03 -0300

    connectiong MySql to dart project

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -1,21 +1,30 @@
+import 'package:mysql1/mysql1.dart';
 import 'package:shelf/shelf.dart';
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
-import 'infra/dependency_injector/dependency_injector.dart';
 import 'infra/dependency_injector/injects.dart';
 import 'infra/middleware_interception.dart';
-import 'infra/security/security_service.dart';
-import 'infra/security/security_service_imp.dart';
-import 'services/noticia_service.dart';
 import 'utils/custom_env.dart';
 
 Future<void> main() async {
   CustomEnv.fromfile('.env-dev');
 
-  final _di = Injects.initialize();
+  var connection = await MySqlConnection.connect(
+    ConnectionSettings(
+      password: await CustomEnv.get<String>(key: 'db_pass'),
+      host: await CustomEnv.get<String>(key: 'db_host'),
+      user: await CustomEnv.get<String>(key: 'db_user'),
+      db: await CustomEnv.get<String>(key: 'db_schema'),
+      port: await CustomEnv.get<int>(key: 'db_port'),
+      characterSet: CharacterSet.UTF8,
+    ),
+  );
 
-  var _securityService = _di.get<SecurityService>();
+  var result = await connection.query('SELECT * FROM usuarios');
+  print(result);
+
+  final _di = Injects.initialize();
 
   var cascadeHandler = Cascade()
       .add(_di.get<LoginApi>().getHandler())
